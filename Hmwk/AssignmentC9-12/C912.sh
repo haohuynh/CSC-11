cc3066a81c2820fd2c501ebfe0e8841179a41b22
Hao Huynh <hah.ptit@gmail.com>
2015-10-07 21:30:50 -0700

    AssignmentC9-12

---
+++ Hmwk/AssignmentC9-12/C912.sh
@@ -0,0 +1,8 @@
+echo "Hello $USER. we are going to run main"
+echo "Today is $(date)"
+echo "Current working directory : $(pwd)"
+echo "GDB is required to see the results in Register R3 and R4"
+rm -vf *.o
+./10a
+./12a
+./12b
