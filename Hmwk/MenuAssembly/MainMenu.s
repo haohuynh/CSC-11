608c1e5b6552b11418cca5d4e28492fa38c08e07
Hao Huynh <hah.ptit@gmail.com>
2015-10-19 22:53:07 -0700

    Menu: 30%

---
+++ Hmwk/MenuAssembly/MainMenu.s
@@ -0,0 +1,112 @@
+/* -- MainMenu.s */
+
+.data
+/* First message */
+.balign 4
+message1: .asciz "Input beginning and end of temperature range:\n"
+
+/* Second message */
+.balign 4
+message2: .asciz "If range is degree Centigrade input 1:\n"
+
+/* Third message */
+.balign 4
+message3: .asciz "If range is degree Fahrenheit input 2:\n"
+
+/* Test message */
+.balign 4
+testMessage: .asciz "Begin range is: %d and End range is: %d"
+
+/* Format pattern for scanf */
+.balign 4
+scan_pattern : .asciz "%d"
+
+/* Where scanf will store the begin range*/
+.balign 4
+beg_range: .word 0
+
+/* Where scanf will store the end range */
+.balign 4
+end_range: .word 0
+
+/* Where scanf will store the choice */
+.balign 4
+choice: .word 0
+
+.balign 4
+return: .word 0
+
+.text
+
+.global main
+main:
+
+ ldr r1, address_of_return /* r1 ? &address_of_return */
+ str lr, [r1] /* *r1 ? lr */
+ 
+ 
+ _do_while_loop:
+ 
+ ldr r0, address_of_message1 /* r0 ? &message1 */
+ bl printf /* call to printf */
+ 
+ ldr r0, address_of_scan_pattern /* r0 ? &scan_pattern */
+ ldr r1, address_of_beg_range /* r1 ? &beg_range */
+ bl scanf /* call to scanf */
+ 
+ ldr r0, address_of_scan_pattern /* r0 ? &scan_pattern */
+ ldr r1, address_of_end_range /* r1 ? &end_range */
+ bl scanf /* call to scanf */
+ 
+ 
+ 
+ ldr r0, address_of_message2 /* r0 ? &message2 */
+ bl printf /* call to printf */
+ 
+ ldr r0, address_of_message3 /* r0 ? &message3 */
+ bl printf /* call to printf */
+ 
+ ldr r0, address_of_scan_pattern /* r0 ? &scan_pattern */
+ ldr r1, address_of_choice /* r1 ? &choice */
+ bl scanf /* call to scanf */
+ 
+ 
+ 
+ ldr r1, address_of_beg_range /* r1 ? &beg_range */
+ ldr r1, [r1] /* r1 ? *r1 */
+ ldr r2, address_of_end_range /* r1 ? &end_range */
+ ldr r2, [r2] /* r1 ? *r1 */
+ ldr r0, address_of_test_message /* r0 ? &message2 */
+ bl printf /* call to printf */
+ 
+ 
+ ldr r0, address_of_choice /* r1 ? &choice */
+ ldr r0, [r0] /* r0 = *r0 */
+ cmp r0, #0
+ ble _exit
+ cmp r0, #3
+ bge _exit
+ 
+ bal _do_while_loop
+  
+ 
+ _exit:
+ 
+ ldr lr, address_of_return /* lr ? &address_of_return */
+ ldr lr, [lr] /* lr ? *lr */
+ bx lr /* return from main using lr */
+ 
+address_of_message1 : .word message1
+address_of_message2 : .word message2
+address_of_message3 : .word message3
+address_of_test_message : .word testMessage
+address_of_scan_pattern : .word scan_pattern
+address_of_beg_range : .word beg_range
+address_of_end_range : .word end_range
+address_of_choice : .word choice
+address_of_return : .word return
+
+/* External */
+.global printf
+.global scanf
+
--- upload.bat
+++ upload.bat
@@ -1,5 +1,5 @@
 git add --all
 git config --global user.name "Hao Huynh"
 git config --global user.email hah.ptit@gmail.com
-git commit -m "Functions"
+git commit -m "Menu: 30%"
 git push origin master
