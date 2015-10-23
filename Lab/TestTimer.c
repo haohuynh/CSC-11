687fc74de2717efaeaa365d21aeb8ed95cd2a137
Hao Huynh <hah.ptit@gmail.com>
2015-10-23 15:38:26 -0700

    Testing: Project1

---
+++ Lab/TestTimer.c
@@ -0,0 +1,21 @@
+/* 
+ * File:   TestTimer.c
+ * Author: HaoHuynh
+ *
+ * Created on October 23, 2015, 3:36 PM
+ */
+
+#include <time.h>
+#include <stdlib.h>
+
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    
+    time(NULL);
+    
+    return (EXIT_SUCCESS);
+}
+
