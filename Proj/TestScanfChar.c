bf5b49ea26d7f1f7f45a3fe01d3a2fbc87dce8cb
Hao Huynh <hah.ptit@gmail.com>
2015-10-23 21:23:40 -0700

    Project1 : 20%

---
+++ Proj/TestScanfChar.c
@@ -0,0 +1,23 @@
+/* 
+ * File:   TestScanfChar.c
+ * Author: HaoHuynh
+ *
+ * Created on October 23, 2015, 9:22 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    
+    char c;
+    
+    scanf("%c",&c);
+            
+    
+    return (EXIT_SUCCESS);
+}
+
