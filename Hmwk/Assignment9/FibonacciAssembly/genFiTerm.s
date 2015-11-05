89f971c6b7ad6002d01cee14f440e01eea48cfd1
Hao Huynh <hah.ptit@gmail.com>
2015-11-04 18:50:08 -0800

    Assignment 9: 60%

--- Hmwk/Assignment9/FibonacciAssembly/genFiTerm.s
+++ Hmwk/Assignment9/FibonacciAssembly/genFiTerm.s
@@ -8,23 +8,23 @@
 genFiTerm:
 
     push {r4, lr}       /* Push r4 and lr onto the stack */
-     
+
     cmp r0, #2         /* compare r0 and 2 */
     bgt gt_2     	   /* if r0 > 2 then branch */
     mov r0, #1         /* if r0 <=2 then return r0 = 1 */
     bal exit
-	
+
 gt_2:
-                 
+
     sub r0, r0, #1     /* r0 = r0 - 1 , find F(n-1)*/
     bl genFiTerm
-	
-	mov r4, r0         /* Copy the F(n-1) in r0 to r4 */
-	
-	sub r0, r0, #2     /* r0 = r0 - 2 , find F(n-2)*/
+
+    mov r4, r0         /* Copy the F(n-1) in r0 to r4 */
+
+    sub r0, r0, #2     /* r0 = r0 - 2 , find F(n-2)*/
     bl genFiTerm
-	
-	add r0, r0, r4 	   /*Return F(n-2) + F(n-1)*/ 	    
+
+    add r0, r0, r4     /*Return F(n-2) + F(n-1)*/
 
 exit:
     pop {r4, lr}       /* Pop lr and r4 from the stack */
