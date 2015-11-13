7e75df1bc2687b9dfaa9d341d4608a890d8e574e
Hao Huynh <hah.ptit@gmail.com>
2015-11-13 11:15:27 -0800

    Collatz Timing: on processing

--- upload.bat
+++ upload.bat
@@ -1,5 +1,5 @@
 git add --all
 git config --global user.name "Hao Huynh"
 git config --global user.email hah.ptit@gmail.com
-git commit -m "ArrayFib: 100%"
+git commit -m "Collatz Timing: on processing"
 git push origin master
