be13531d7185708547ebdb678dc6420e3e2a2ab5
Hao Huynh <hah.ptit@gmail.com>
2015-11-16 10:15:47 -0800

    Stack Fib: 10%

---
+++ Hmwk/StackFib/getStackFibTerm.s
@@ -0,0 +1,39 @@
+/*-- getStackFibTerm.s : generates the first 45 term of the Fibonacci Sequence and stores them in the Stack of this program */
+
+.data
+
+/* A return address for this function */
+.balign 4
+return: .word 0
+
+.text
+
+.global getStackFibTerm
+getStackFibTerm:
+	ldr r0, =return /* Save the return address*/
+	str lr, [r0]
+    	
+	mov r0, #1 /*Store the first Fibonacci term to the first element */
+    mov r1, #1 /*Store the second Fibonacci term to the second element */
+    push {r0, r1}
+ 
+	mov r0, #3 /*r0 is now the index of a element in the Fibonacci array*/
+ 
+	_for_loop:
+ 
+	cmp r0, #45 /*Check if r0 > 45 : the size of the array*/
+	bgt _done_for_loop
+ 	
+	pop {r1, r2}	/* r1 = F(n-2), r2 = F(n-1) */
+	add r3, r1, r2 /* r3 = F(n-2) + F(n-1) */
+	push {r1, r2, r3}	/* Store F(n-2),F(n-1), and F(n) back to the stack */ 
+
+	add r0, r0, #1 /* r0 <- (r0 + 1) */
+	bal _for_loop
+ 
+	_done_for_loop:
+    
+    ldr lr, =return /*Load the return address*/
+	ldr lr, [lr]
+    bx lr              /* Leave getStackFibTerm */
+	
---
+++ Hmwk/StackFib/main.s
@@ -0,0 +1,88 @@
+/* -- main.s */
+
+.data
+
+/* The welcome message */
+.balign 4
+wel_mess: .asciz "\nPlease enter a position in the Fibonacci sequence [1-45]: "
+
+/* The result message */
+.balign 4
+result_mess: .asciz "The result is: %d"
+
+/* A numeric format pattern for scanf */
+.balign 4
+n_scan_pattern : .asciz "%d"
+
+/* Where scanf will store the n th*/
+.balign 4
+n_term: .word 0
+
+/* The array contains the first 45 terms in the Fibonacci Sequence*/
+.balign 4
+fArry: .skip 180
+
+.text
+
+.global main
+main:
+
+ push {r4, lr} /*Store the address of the next instruction after this main*/
+ 
+ ldr r0, fArry_addr /* r0 is the pointer pointing to the beginning of fArry */
+ bl getStackFibTerm /* Call getStackFibTerm function*/
+  
+ _do_while_loop:
+ 
+ ldr r0, wel_mess_addr /* r0 <- &wel_mess*/
+ bl printf /* call to printf */	
+ 
+ ldr r0, n_scan_pattern_addr /* r0 <- &n_scan_pattern*/
+ ldr r1, n_term_addr /* r1 <- &n_term*/
+ bl scanf /* call to scanf */
+ 
+ ldr r0, n_term_addr /* r0 <- &n_term*/
+ ldr r0, [r0] /* r0 <- *r0 */
+ 
+ cmp r0,#0 /* check if r0 <= 0*/
+ ble _exit
+ 
+ cmp r0,#45 /* check if r0 >=45*/
+ bgt _process_over_size
+ 
+ ldr r1, fArry_addr /* The starting pointer of the Fibonacci Array */
+ mov r2, #4 /* r2 = 4 (bytes) */
+ mov r3, #45 /* r3 : the max index value in the Fibonacci Sequence */
+ sub r3, r3, r0 /* r3 : the n term counting backward from 45 */
+ add r3, r3, #1
+ mul r0, r3, r2 /* r0 = backward-index * 4 (bytes) : current position in the Fibonacci Array */
+ 
+ ldr r1, [r1, +r0] /* r1 contains the value */
+ bal _output
+ 
+ _process_over_size:
+ mov r1, #0 /*Output 0 the over_size case */
+ 
+ _output:
+ ldr r0, result_mess_addr /* r0 <- &result_mess*/
+ bl printf /* call to printf */	
+ 
+ bal _do_while_loop 
+  
+ _exit: 
+ pop {r4, lr} /*Load the address of the next instruction*/
+ bx lr /* return from main using lr */
+ 
+/*Addresses Referencing*/
+wel_mess_addr : .word wel_mess
+result_mess_addr : .word result_mess
+n_scan_pattern_addr : .word n_scan_pattern
+n_term_addr : .word n_term
+fArry_addr : .word fArry
+
+/* External Functions*/
+.global printf
+.global scanf
+.global getStackFibTerm
+
+
